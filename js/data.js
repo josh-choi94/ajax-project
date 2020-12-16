909c355686dbebcff574cab7f027bea3a46e4c29
josh choi <wonchoi1994@gmail.com>
2020-12-16 15:12:18 -0800

    api xmlhttp added

--- js/data.js
+++ js/data.js
@@ -1,1 +1,13 @@
 /* exported data */
+function playerStats() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'GET https://www.balldontlie.io/api/v1/stats');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      console.log(xhr.status);
+      console.log(xhr.response);
+    });
+    xhr.send();
+  }
+  playerStats();
+  
